b12a68ffdc2782330f359a2c6b3ce4cb96a11652
Yogesh <yogesh@lonkar.org>
2024-05-23 17:12:36 +0200

    Use GITHUB_API_URL for Octokit clients (#100)
    API calls on GHES can be supported by using the GITHUB_API_URL
    environment variable. However artifact client doesn't support GHES so
    outputs-from feature can't be used on GHES yet.
    
    Closes #99

--- lib/miscellaneous/sleep.test.ts
+++ lib/miscellaneous/sleep.test.ts
@@ -7,5 +7,5 @@ jest.mock("@actions/core");
 test("sleep debug logs label", async () => {
     const controller = new AbortController();
     await expect(sleep(2, controller, "label-1")).resolves.toBeUndefined();
-    expect(debug).toBeCalledWith("label-1 done");
+    expect(debug).toHaveBeenCalledWith("label-1 done");
 });
