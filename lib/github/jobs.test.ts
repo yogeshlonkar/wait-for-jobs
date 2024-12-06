f68662ce66f5cf20f1fb1c143bf3c9f6c30ef0d0
Oscar Choi <oscar@goodnotesapp.com>
2024-12-06 16:22:36 +0800

    Use paginate to return all jobs instead of only 30

--- lib/github/jobs.test.ts
+++ lib/github/jobs.test.ts
@@ -5,12 +5,14 @@ import jobsResponse from "./__fixtures__/jobs.json";
 import { getCurrentJobs } from "./jobs.js";
 
 const listJobsForWorkflowRunAttempt = vi.fn();
+const paginate = vi.fn();
 const octokit = vi.fn();
 
 vi.mock("@octokit/rest", () => ({
     Octokit: {
         plugin: () =>
             vi.fn().mockImplementation(() => ({
+                paginate,
                 actions: {
                     listJobsForWorkflowRunAttempt
                 }
@@ -20,6 +22,7 @@ vi.mock("@octokit/rest", () => ({
 
 beforeEach(() => {
     listJobsForWorkflowRunAttempt.mockClear();
+    paginate.mockClear();
     process.env.GITHUB_REPOSITORY = "yogeshlonkar/wait-for-job";
     process.env.GITHUB_RUN_ID = "10";
     process.env.GITHUB_RUN_ATTEMPT = "1";
@@ -33,19 +36,10 @@ afterEach(() => {
 });
 
 describe("getCurrentJobs", () => {
-    test("throws error on failed listJobsForWorkflowRunAttempt", async () => {
-        listJobsForWorkflowRunAttempt.mockResolvedValue({
-            status: 404
-        });
-        await expect(getCurrentJobs("some-auth")).rejects.toThrow(
-            new Error(`unexpected status from api.github.com: 404`)
-        );
-    });
-
     test("returns Run", async () => {
-        listJobsForWorkflowRunAttempt.mockResolvedValue(jobsResponse);
+        paginate.mockResolvedValue(jobsResponse.data.jobs);
         await expect(getCurrentJobs("some-auth")).resolves.toEqual(jobsResponse.data);
-        expect(listJobsForWorkflowRunAttempt).toHaveBeenCalledWith({
+        expect(paginate).toHaveBeenCalledWith(listJobsForWorkflowRunAttempt, {
             attempt_number: 1,
             owner: "yogeshlonkar",
             repo: "wait-for-job",
